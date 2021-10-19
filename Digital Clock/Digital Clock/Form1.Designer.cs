18ea75661200ae6a63bc22f8d8962f92a86d5bda
tyree-nevels <92060097+tyree-nevels@users.noreply.github.com>
2021-10-18 21:54:31 -0700

    Add files via upload

---
+++ Digital Clock/Digital Clock/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Digital_Clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(61, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(666, 156);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Clock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digital Clock";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Digital Clock/Digital Clock/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Clock : Form
+    {
+        Timer t = new Timer();
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int hours = DateTime.Now.Hour;
+            int minutes = DateTime.Now.Minute;
+            int seconds = DateTime.Now.Second;
+            string Time = "";
+
+            if (hours < 10)
+            {
+                Time += "0" + hours;
+            }
+            else
+            {
+                Time += hours;
+            }
+            Time += ":";
+            if (minutes < 10)
+            {
+                Time += "0" + minutes;
+            }
+            else
+            {
+                Time += minutes;
+            }
+            Time += ":";
+            if (seconds < 10)
+            {
+                Time += "0" + seconds;
+            }
+            else
+            {
+                Time += seconds;
+            }
+
+            label1.Text = Time;
+        }
+    }
+}
